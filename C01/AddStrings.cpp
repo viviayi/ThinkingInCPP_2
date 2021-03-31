4d003e0a3ef8c48d54da278d166b8ebead813b24
viviayi <741173912@qq.com>
2021-03-31 18:02:14 +0800

    Create AddStrings.cpp

---
+++ C01/AddStrings.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main(){
+    string s1("This ");
+    string s2("That ");
+    string s3("The other ");
+    // operator+ concatenates strings
+    s1 = s1 + s2;
+    cout << s1 << endl;
+    // Another way to concatenates strings
+    s1 += s3;
+    cout << s1 << endl;
+    // You can index the string on the right
+    s1 += s3 + s3[4] + "oh lala";
+    cout << s1 << endl;
+} ///:~
+
+/*
+This That
+This That The other
+This That The other The other ooh lala
+*/
