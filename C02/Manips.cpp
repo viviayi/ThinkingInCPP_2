d6623e11c09f14bffd38aa214d35a59aa8f2b9bc
viviayi <741173912@qq.com>
2021-08-11 20:00:27 +0800

    Create Manips.cpp

---
+++ C02/Manips.cpp
@@ -0,0 +1,53 @@
+// Format.cpp using manipulators
+#include <fstream>
+#include <iomanip>
+using namespace std;
+
+int main(){
+    ofstream trc("trace.out");
+    int i = 47;
+    float f = 2300114.414159;
+    char* s = "Is there any more?";
+
+    trc << setiosflags(
+        ios::unitbuf /*| ios::stdio */ /// ?????
+        | ios::showbase | ios::uppercase
+        | ios::showpos);
+    trc << i << endl; // Default to dec
+    trc << hex << i << endl;
+    trc << resetiosflags(ios::uppercase) << oct << i << endl;
+    trc.setf(ios::left, ios::adjustfield);
+    trc << resetiosflags(ios::showpos) << dec << setfill('0');
+    trc << "fill char: " << trc.fill() << endl;
+    trc << setw(10) << i << endl;
+    trc.setf(ios::right, ios::adjustfield);
+    trc << setw(10) << i << endl;
+    trc.setf(ios::internal, ios::adjustfield);
+    trc << setw(10) << i << endl;
+    trc << i << endl; // Without setw(10)
+
+    trc << resetiosflags(ios::showpos) << setiosflags(ios::showpoint)
+      << "prec = " << trc.precision() << endl;
+    trc.setf(ios::scientific, ios::floatfield);
+    trc << f << endl;
+    trc.setf(ios::fixed, ios::floatfield);
+    trc << f << endl;
+    trc.setf(ios::floatfield, ios::floatfield);
+    trc << f << endl;
+    trc << setprecision(20);
+    trc << "prec = " << trc.precision() << endl;
+    trc << f << endl;
+    trc.setf(ios::scientific, ios::floatfield);
+    trc << f << endl;
+    trc.setf(ios::fixed, ios::floatfield);
+    trc << f << endl;
+    trc.setf(ios::floatfield, ios::floatfield);
+    trc << f << endl;
+
+    trc << setw(10) << s << endl;
+    trc << setw(40) << s << endl;
+    trc.setf(ios::left, ios::adjustfield);
+    trc << setw(40) << s << endl;
+
+    trc << resetiosflags(ios::showpoint | ios::unitbuf);
+} ///:~
